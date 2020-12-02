f694c3bf77822e8d417715485fc94ec5a6667e1d
gabel35 <gabrieljose3135@gmail.com>
2020-12-01 22:52:46 -0500

    figured out the aligning of the #timer - thank you Christian

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// alert ("Hi");
+var timerEl = document.getElementById("#timerEl");
+
+var countdownTimer = 90;
+
+function timerCountdown() {
+  var timerInterval = setInterval(function() {
+    countdownTimer--;
+    timerEl.textContent = "Time: " + countdownTimer;
+
+    if(countdownTimer === 0) {
+      clearInterval(timerInterval);
+      resultPage();
+    }
+
+  }, 1000);
+}
+
+timerCountdown()
