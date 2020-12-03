2e5cbcef23fee7755e2700abd873bb0a8355739a
gabel35 <gabrieljose3135@gmail.com>
2020-12-02 20:49:49 -0500

    started the javascript, moved the html starter text into the javascript, and added the rest of the ids/html I will need

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-var timerEl = document.getElementById("#timerEl");
+var timerEl = document.getElementById("timerEl");
+var titleHeading = document.getElementById("titleHeading");
+var questionHeading = document.getElementById("questionHeading");
+var welcomeP = document.getElementById("welcomeP");
+var startQuiz = document.getElementById("startQuiz");
+var optionOne = document.getElementById("optionOne");
+var optionTwo = document.getElementById("optionTwo");
+var optionThree = document.getElementById("optionThree");
+var optionFour = document.getElementById("optionFour");
 
 var countdownTimer = 90;
 
 function timerCountdown() {
   var timerInterval = setInterval(function() {
     countdownTimer--;
-    timerEl.textContent = "Time: " + countdownTimer;
+    timerEl.textContent = "Time Remaining: " + countdownTimer;
 
     if(countdownTimer === 0) {
       clearInterval(timerInterval);
@@ -15,4 +23,12 @@ function timerCountdown() {
   }, 1000);
 }
 
-timerCountdown()
+// timerCountdown()
+
+function startPage() {
+  titleHeading.textContent = "Javascript Coding Assessment";
+  welcomeP.textContent = "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will be penalized by deducting ten seconds from your time.";
+  startQuiz.textContent = "Start Quiz";
+}
+
+startPage()
