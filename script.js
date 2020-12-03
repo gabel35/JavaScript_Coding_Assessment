3c0b794dc46316dd5cdeb2b929d2944872930ba3
gabel35 <gabrieljose3135@gmail.com>
2020-12-02 21:10:58 -0500

    added some pseudocode to work off of

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
+//getting all the ids from the HTML to be used later//
 var timerEl = document.getElementById("timerEl");
 var titleHeading = document.getElementById("titleHeading");
 var questionHeading = document.getElementById("questionHeading");
 var welcomeP = document.getElementById("welcomeP");
-var startQuiz = document.getElementById("startQuiz");
+var theBtn = document.getElementById("theBtn");
 var optionOne = document.getElementById("optionOne");
 var optionTwo = document.getElementById("optionTwo");
 var optionThree = document.getElementById("optionThree");
 var optionFour = document.getElementById("optionFour");
 
+//creating the timer/countdown to be displayed through quiz//
 var countdownTimer = 90;
 
 function timerCountdown() {
@@ -23,12 +25,17 @@ function timerCountdown() {
   }, 1000);
 }
 
-// timerCountdown()
+// timerCountdown() <-- will use this later -->
 
+//creating the starter page//
 function startPage() {
   titleHeading.textContent = "Javascript Coding Assessment";
   welcomeP.textContent = "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will be penalized by deducting ten seconds from your time.";
-  startQuiz.textContent = "Start Quiz";
+  theBtn.textContent = "Start Quiz";
 }
 
 startPage()
+
+//create the questions (maybe 5 with 4 options each)//
+//each question will have 4 buttons (options) - 1 correct answer, that will move on without penalty and 3 that will move on and decrease 10 seconds from the timer//
+//results page that will record the time remaining as the "final score/highscore" and allow player to record their name in a text box, plus the button that leads to the highscore page//
